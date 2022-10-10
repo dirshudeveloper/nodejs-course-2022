91408535b81e5b57c7dc11a2892fa017107ed050
dirshudeveloper <87856343+dirshudeveloper@users.noreply.github.com>
2022-10-10 22:28:28 +0300

    removed node-modules

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+ 
+const {area,circumference} = require("./circle"); 
+const PORT = 4000;
 const http = require("http");
 
 const server = http.createServer((req,res) =>{
     res.write("hello node");
     res.end();
 })
-server.listen(3000);
+console.log(`server is running at http://localhost:${PORT}`);
+server.listen(PORT);
---
+++ circle.js
@@ -0,0 +1,11 @@
+exports.area = function(r){
+    return Math.PI * r * r;
+}
+
+exports.circumference = function(r){
+    return 2 * Math.PI * r;
+}
+
+
+
+ 
